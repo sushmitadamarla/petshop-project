e006983c58d0fbd222a2620538d7b069c36aa541
sushmitadamarla <sushmitadamarla09@gmail.com>
2026-04-17 11:11:16 +0530

    added security

--- src/main/java/com/petshop/config/SecurityConfig.java
+++ src/main/java/com/petshop/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.petshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,8 +15,9 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
